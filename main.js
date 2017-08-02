8cc678b53c0a51ceb27ef2d06d4d4c8ac7d3cfff
Angela Vogler <angelavogler4@gmail.com>
2017-08-02 14:21:01 -0400

    created profile template

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
-const profiles = `
-<header>
-  <h1>International Company Directory</h1>
-</header>
+for (let i = 0; i < customers.results.length; i++) {
 
-<section>
-</section>
-`;
+  const profiles = `
+  <header>
+    <h1>International Company Directory</h1>
+  </header>
 
-const container = document.querySelector("body");
-container.innerHTML = profiles;
+  <section>
+    <div class="user-profile">
+      <img class="user-photo" src=${customers.results[i].picture.large}>
+      <h3 class="user-name">${customers.results[i].name.first} ${customers.results[0].name.last}</h3>
+      <div class="user-email">
+        ${customers.results[i].email}
+      </div>
+      <ul class="user-contact">
+        <li>${customers.results[i].location.street}</li>
+        <li>${customers.results[i].location.city}, ${customers.results[i].location.state} ${customers.results[i].location.postcode}</li>
+        <li>${customers.results[i].cell}</li>
+      </ul>
+      <div class="user-ssn">
+        ${customers.results[i].id.value}
+      </div>
+    </div
+  </section>
+  `;
+
+  const container = document.querySelector("body");
+  container.innerHTML = profiles;
+}
+
+// const profiles = `
+// <header>
+//   <h1>International Company Directory</h1>
+// </header>
+//
+// <section>
+//   <div class="user-profile">
+//     <img class="user-photo" src=${customers.results[0].picture.large}>
+//     <h3 class="user-name">${customers.results[0].name.first} ${customers.results[0].name.last}</h3>
+//     <div class="user-email">
+//       ${customers.results[0].email}
+//     </div>
+//     <ul class="user-contact">
+//       <li>${customers.results[0].location.street}</li>
+//       <li>${customers.results[0].location.city}, ${customers.results[0].location.state} ${customers.results[0].location.postcode}</li>
+//       <li>${customers.results[0].cell}</li>
+//     </ul>
+//     <div class="user-ssn">
+//       ${customers.results[0].id.value}
+//     </div>
+//   </div
+// </section>
+// `;
+
+// const container = document.querySelector("body");
+// container.innerHTML = profiles;
