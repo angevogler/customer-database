5afe00dd49d7ce3581492f17000f9107d21e3b4b
Angela Vogler <angelavogler4@gmail.com>
2017-08-02 16:04:58 -0400

    began adding styiling - 4pm turn in

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+let superLiteral = '';
+// const title = document.createElement("h1");
+
 for (let i = 0; i < customers.results.length; i++) {
 
   const profiles = `
-  <header>
-    <h1>International Company Directory</h1>
-  </header>
 
   <section>
     <div class="user-profile">
       <img class="user-photo" src=${customers.results[i].picture.large}>
-      <h3 class="user-name">${customers.results[i].name.first} ${customers.results[0].name.last}</h3>
+      <h3 class="user-name">${customers.results[i].name.first} ${customers.results[i].name.last}</h3>
       <div class="user-email">
         ${customers.results[i].email}
       </div>
@@ -24,33 +24,8 @@ for (let i = 0; i < customers.results.length; i++) {
   </section>
   `;
 
-  const container = document.querySelector("body");
-  container.innerHTML = profiles;
+  superLiteral += profiles;
 }
-
-// const profiles = `
-// <header>
-//   <h1>International Company Directory</h1>
-// </header>
-//
-// <section>
-//   <div class="user-profile">
-//     <img class="user-photo" src=${customers.results[0].picture.large}>
-//     <h3 class="user-name">${customers.results[0].name.first} ${customers.results[0].name.last}</h3>
-//     <div class="user-email">
-//       ${customers.results[0].email}
-//     </div>
-//     <ul class="user-contact">
-//       <li>${customers.results[0].location.street}</li>
-//       <li>${customers.results[0].location.city}, ${customers.results[0].location.state} ${customers.results[0].location.postcode}</li>
-//       <li>${customers.results[0].cell}</li>
-//     </ul>
-//     <div class="user-ssn">
-//       ${customers.results[0].id.value}
-//     </div>
-//   </div
-// </section>
-// `;
-
-// const container = document.querySelector("body");
-// container.innerHTML = profiles;
+  const body = document.querySelector("body");
+  body.innerHTML += `<h1 class="header">Internal Company Directory</h1>`;
+  body.innerHTML += superLiteral;
