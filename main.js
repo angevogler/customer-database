673c43661aa905fa55db191ae4495ff969685609
Angela Vogler <angelavogler4@gmail.com>
2017-08-02 13:42:32 -0400

    created and linked main.css and main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const profiles = `
+<header>
+  <h1>International Company Directory</h1>
+</header>
+
+<section>
+</section>
+`;
+
+const container = document.querySelector("body");
+container.innerHTML = profiles;
